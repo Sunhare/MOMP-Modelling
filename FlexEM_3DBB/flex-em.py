81d9553d3e1e6353be83766a2a194a45de76ce68
Sunhare <bgyoume@gmail.com>
2018-11-30 17:19:21 -0800

    Updated for wei 308 model

--- FlexEM_3DBB/flex-em.py
+++ FlexEM_3DBB/flex-em.py
@@ -16,41 +16,34 @@ from CG import opt_cg
 from MD import opt_md
 
 env = environ()
-#Things to try
-"""
-Fit SvF5LDV in 1ake mrc - if fail, problem in rigid or pdb
-Fit mdl1_fit in 30_monomer - if fail, problem in mrc
-
-"""
 
 ############### INPUT PARAMETERS ##################
-optimization = 'MD'                     # type of optimization: CG / MD
-input_pdb_file = '3dBB_mod10_trimer_fit.pdb'         #5LDV
-# input_pdb_file = 'MOMP_SvF_5ldv_wrongfit.pdb'         #AKE
-# input_pdb_file = 'mdl1_fit.pdb'          # input model for optimization
+optimization = 'MD'                                # type of optimization: CG / MD
+input_pdb_file = '3dBB_trimer_fit_308.pdb'         # input model for optimization
+code = '3DBB'                                      # 4 letter code of the structure          
+em_map_file = '308.mrc'                            # name of EM density map (mrc)
+format='MRC'                                       # map format: MRC or XPLOR
 
-code = 'CTMC'    #5LDV                       # 4 letter code of the structure
-# code = '5ake' #AKE
-# em_map_file = 'wei_momp.mrc'  #5LDV               # name of EM density map (mrc)
-em_map_file = 'wei_momp.mrc' #AKE
-# em_map_file = '1akeA_10A.mrc' #AKE
-format='MRC'                            # map format: MRC or XPLOR
-# apix=2.7                                  # voxel size: A/pixel #5LDV
-apix = 2.7 #AKE
-# box_size=128                             # size of the density map (cubic) #5LDV
-box_size = 128 #AKE
-resolution=8.0                         # resolution (A)
-# x= -6.494; y=13.381; z=-5.410 #AKE		                # origin of the map (A)
-# x=-54*(apix*-1); y=-57*(apix*-1); z=-46*(apix*-1) #5LDV
-x= 0; y=0; z=0
-path = './'	                        # path to work directory
-init_dir = 1                            # number of the initial directory 
-num_of_runs = 1                         # number of flex-em CG runs 
-num_of_iter = 5                         # number of iterations in each MD run 
-          				# (default=4) 
-# rigid_filename = 'rigid_1ake.txt'            # rigid bodies file name #AKE
-# rigid_filename = 'rigid.txt'            # rigid bodies file name #5LDV
-rigid_filename = '3dBB_rigid.txt'            # rigid bodies file name #AKE
+apix = 2.2                                         # voxel size: A/pixel 
+box_size = 128                                     # size of the density map (cubic)
+resolution=12.0                                    # resolution (A)
+x= 0; y=0; z=0                                     # coordinates of map/model (make sure they're fit)
+
+path = './'	                                       # path to work directory
+init_dir = 2                                       # number of the initial directory 
+num_of_runs = 1                                    # number of flex-em CG runs 
+num_of_iter = 5                                    # number of iterations in each MD run (default=4) 
+          				
+#MOST OF THE PROBLEMS ARE WITH THIS FILE
+rigid_filename = '3dBB_rigid_308.txt'            # rigid bodies file name #AKE
+
+
+
+
+
+
+
+
 ############### RUN OPTIMIZATION ##################
 path = os.path.abspath(path)
 
